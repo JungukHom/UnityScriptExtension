657a77db70852e6e1a5397c5937efec41571a377
JungukHom <developer0223@gmail.com>
2019-04-05 11:34:40 +0900

    Initial commit.
    Initial commit.

---
+++ Project/UnityScriptExtension/Assets/Scripts/Utility/Extensions/CSharpStructExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace developer0223.Utility
+{
+    /// <summary>
+    /// C# structs extension class
+    /// 
+    /// Developer Information
+    /// GitHub : https://github.com/jungukhom
+    /// Blog : https://developer0223.tistory.com/
+    /// </summary>
+
+    public static class CSharpStructExtensions
+    {
+        #region string (System.String)
+        public static bool IsNullOrEmpty(this string @string)
+        {
+            return @string == string.Empty || @string == null;
+        }
+
+        public static bool IsNumber(this string @string)
+        {
+
+        }
+        #endregion
+    }
+}
