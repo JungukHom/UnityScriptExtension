4b267e6e0b2cfbfa93262910cfb0c433d19ac324
JungukHom <developer0223@gmail.com>
2019-04-09 15:01:15 +0900

    Add | CustomClasses namespace
    Added CustomDictionary<TKey, TValue> class.

--- Project/UnityScriptExtension/Assets/Scripts/Test.cs
+++ Project/UnityScriptExtension/Assets/Scripts/Test.cs
@@ -5,13 +5,28 @@
 
     // Utility
     using developer0223.Utility.Extensions;
+    using developer0223.Utility.CustomClasses;
 
     public class Test : MonoBehaviour
     {
         private void Start()
         {
-            float test = 1.5f;
-            Debug.Log(test.ToRoundInteger());
+            CustomDictionary<int, string> dic_01 = new CustomDictionary<int, string>();
+            dic_01.Add(0, "zero");
+            dic_01.Add(1, "one");
+            dic_01.Add(2, "two");
+            dic_01.Add(3, "three");
+
+            CustomDictionary<int, string> dic_02 = new CustomDictionary<int, string>();
+            dic_02.Add(2, "two");
+            dic_02.Add(3, "three");
+            dic_02.Add(4, "four");
+
+            CustomDictionary<int, string> plus = (dic_01 + dic_02) - (dic_01 & dic_02);
+            foreach(int key in plus.Keys)
+            {
+                Debug.Log($"{key} : {plus[key]}");
+            }
         }
     }
 }
---
+++ Project/UnityScriptExtension/Assets/Scripts/Utility/CustomClasses/CustomDictionary.cs
@@ -0,0 +1,60 @@
+﻿namespace developer0223.Utility.CustomClasses
+{
+    // C#
+    using System.Collections.Generic;
+
+    public class CustomDictionary<TKey, TValue> : Dictionary<TKey, TValue>
+    {
+        public static CustomDictionary<TKey, TValue> operator &(CustomDictionary<TKey, TValue> a, CustomDictionary<TKey, TValue> b)
+        {
+            CustomDictionary<TKey, TValue> result = new CustomDictionary<TKey, TValue>();
+            foreach (TKey aCollection in a.Keys)
+            {
+                if (b.ContainsKey(aCollection))
+                {
+                    result.Add(aCollection, a[aCollection]);
+                }
+            }
+
+            return result;
+        }
+
+        public static CustomDictionary<TKey, TValue> operator +(CustomDictionary<TKey, TValue> a, CustomDictionary<TKey, TValue> b)
+        {
+            CustomDictionary<TKey, TValue> result = new CustomDictionary<TKey, TValue>();
+            foreach (TKey aCollection in a.Keys)
+            {
+                result.Add(aCollection, a[aCollection]);
+            }
+
+            foreach (TKey bCollection in b.Keys)
+            {
+                if (!result.ContainsKey(bCollection))
+                {
+                    result.Add(bCollection, b[bCollection]);
+                }
+            }
+
+            return result;
+        }
+
+        public static CustomDictionary<TKey, TValue> operator -(CustomDictionary<TKey, TValue> a, CustomDictionary<TKey, TValue> b)
+        {
+            CustomDictionary<TKey, TValue> result = new CustomDictionary<TKey, TValue>();
+            foreach (TKey aCollection in a.Keys)
+            {
+                result.Add(aCollection, a[aCollection]);
+            }
+
+            foreach (TKey bCollection in b.Keys)
+            {
+                if (result.ContainsKey(bCollection))
+                {
+                    result.Remove(bCollection);
+                }
+            }
+
+            return result;
+        }
+    }
+}
