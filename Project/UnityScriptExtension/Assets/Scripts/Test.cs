0df44f359c2b9bf3c05708ea7059ec48c7abd966
JungukHom <developer0223@gmail.com>
2019-04-30 16:29:00 +0900

    Add | Utility.Math

--- Project/UnityScriptExtension/Assets/Scripts/Test.cs
+++ Project/UnityScriptExtension/Assets/Scripts/Test.cs
@@ -10,40 +10,9 @@
 
     public class Test : MonoBehaviour
     {
-        public UnityEngine.UI.Text text;
-
         private void Start()
         {
-            //CustomDictionary<int, string> dic_01 = new CustomDictionary<int, string>
-            //{
-            //    { 0, "zero" },
-            //    { 1, "one" },
-            //    { 2, "two" },
-            //    { 3, "three" }
-            //};
 
-            //CustomDictionary<int, string> dic_02 = new CustomDictionary<int, string>
-            //{
-            //    { 2, "two" },
-            //    { 3, "three" },
-            //    { 4, "four" }
-            //};
-
-            //CustomDictionary<int, string> plus = (dic_01 | dic_02) - (dic_01 & dic_02);
-            //foreach(int key in plus.Keys)
-            //{
-            //    Debug.Log($"{key} : {plus[key]}");
-            //}
-
-            //Debug.Log(plus.ToString());
-
-            Color a = Utility.Normalize255RGB(400, 400, 400);
-            Color b = Utility.Normalize255RGB(255, 255, 255, 255);
-
-            Debug.Log(a);
-            Debug.Log(b);
-
-            text.color = a;
         }
     }
 }
---
+++ Project/UnityScriptExtension/Assets/Scripts/Utilities/Test/Utility.Math.cs
@@ -0,0 +1,14 @@
+﻿namespace developer0223.Utilities
+{
+    using System;
+
+    using UnityEngine;
+
+    public static partial class Utility
+    {
+        public static T Add<T>(params T[] values) where T : IEquatable<T>
+        {
+            
+        }
+    }
+}
