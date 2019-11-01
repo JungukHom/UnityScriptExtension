4590760725870ef260d078aac454619daa1d37ef
JungukHom <developer0223@gmail.com>
2019-11-01 13:46:04 +0900

    Add | IPoolableObject

---
+++ Project/UnityScriptExtension/Assets/Scripts/Utilities/IPoolableObject.cs
@@ -0,0 +1,20 @@
+﻿namespace developer0223
+{
+    // Unity
+    using UnityEngine;
+
+    public interface IPoolableObject
+    {
+        bool GetActived();
+
+        GameObject GetReferenceObject();
+
+        void Active();
+
+        void Active(Vector3 position);
+
+        void Active(Vector3 position, Quaternion rotation, Vector3 scale);
+
+        void DeActive();
+    }
+}
