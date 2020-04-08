ef156256d45e393539b0d6fb43ad5140b923b914
JungukHom <developer0223@gmail.com>
2020-04-08 14:04:06 +0900

    Update | Utility.Math.cs

--- Project/UnityScriptExtension/Assets/Scripts/Utilities/Utility.Math.cs
+++ Project/UnityScriptExtension/Assets/Scripts/Utilities/Utility.Math.cs
@@ -8,5 +8,15 @@
 
     public partial class Utility // .Math
     {
+        public static Vector2 InterpolateToGridPosition(Vector2 touchPosition)
+        {
+            float preX = touchPosition.x;
+            float preY = touchPosition.y;
+
+            int aftX = Mathf.RoundToInt(preX);
+            int aftY = Mathf.RoundToInt(preY);
+
+            return new Vector2(aftX, aftY);
+        }
     }
 }
